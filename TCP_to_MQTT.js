96007cff746bbfbc43cae8e33459c57bf907839a
wegner15 <wigsnah@gmail.com>
2022-03-19 18:52:29 +0300

    TCP_to_MQTT added

---
+++ TCP_to_MQTT.js
@@ -0,0 +1,31 @@
+const mqtt = require('mqtt')
+
+const host = 'broker.emqx.io'
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+const connectUrl = `mqtt://${host}:${port}`
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: 'nash',
+  password: '1234',
+  reconnectPeriod: 1000,
+})
+
+const topic = 'my_data'
+client.on('connect', () => {
+  console.log('Connected')
+  client.subscribe([topic], () => {
+    console.log(`Subscribe to topic '${topic}'`)
+  })
+  client.publish(topic, 'my_data', { qos: 0, retain: false }, (error) => {
+    if (error) {
+      console.error(error)
+    }
+  })
+})
+client.on('message', (topic, payload) => {
+  console.log('Received Message:', topic, payload.toString())
+})
